28549a3b1865e5ac2eb30a8c49adb42ee7cf48a7
jesvica <jvictoriaw@hotmail.co.uk>
2021-12-17 23:50:16 +0000

    finished styling portfolio page

---
+++ script.js
@@ -0,0 +1,17 @@
+function aboutMe() {
+    var x = document.getElementById("aboutMe");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function resume() {
+    var x = document.getElementById("resume");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
